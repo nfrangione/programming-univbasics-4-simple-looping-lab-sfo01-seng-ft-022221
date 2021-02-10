b84da71af6002fde879e7c23b5194d750e37e5c2
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-10 22:20:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,29 @@
-# Write your methods here
+def loop_message_five_times(string)
+  5.times do
+    puts string
+  end
+end
+
+def loop_message_n_times (string, n)
+  n.times do
+    puts string
+  end
+end
+
+def output_array(array)
+  counter = 0 
+  while counter < array.length do
+    puts array[counter]
+    counter += 1 
+  end
+end
+
+def return_string_array(array)
+  string_array = []
+  counter = 0 
+  while counter < array.length do
+    next_element = "#{array[counter]}"
+    string_array[counter] = next_element
+  end
+  string_array
+end
